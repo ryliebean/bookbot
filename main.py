63489b9c39168dddab23d41702e649f9b5db0a6f
ryliebean <ryliejager@gmail.com>
2025-04-24 15:59:16 -0600

    add character counter to stats.py - update main.py to call

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_counter
+from stats import word_counter, char_counter
 
 def get_book_text(title_path):
     with open(title_path) as f:
@@ -6,9 +6,9 @@ def get_book_text(title_path):
     return file_contents
 
 def main():
-    #x = word_counter("hello from word counter")
-    x = word_counter(get_book_text("books/frankenstein.txt"))
-    print(f"{x} words found in the document")
-
+    word_count = word_counter(get_book_text("books/frankenstein.txt"))
+    char_count = char_counter(get_book_text("books/frankenstein.txt"))
+    print(f"{word_count} words found in the document")
+    print(char_count)
 
 main()
--- stats.py
+++ stats.py
@@ -5,3 +5,13 @@ def word_counter(word_string):
     for words in word_string.split():
         count += 1
     return count
+
+def char_counter(word_string):
+    char_list = {}
+    word_string.split()
+    for char in word_string.lower():
+        if char in char_list:
+            char_list[char] += 1
+        else:
+            char_list[char] = 1
+    return char_list
