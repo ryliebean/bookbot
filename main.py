4dee2a35daa7bc4020b15500f67b3b193b3269fc
ryliebean <ryliejager@gmail.com>
2025-04-28 14:09:15 -0600

    add ability to get book title from user input

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from stats import word_counter, char_counter, chars_to_sorted_list
+import sys
 
 def get_book_text(title_path):
     with open(title_path) as f:
         file_contents = f.read()
     return file_contents
 
-
+def user_input():
+    if len(sys.argv) != 2:
+        raise Exception ("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        location = sys.argv[1]
+    return location
 
 def main():
-    text_location = "books/frankenstein.txt"
+    text_location = user_input()
     text_full = get_book_text(text_location)
     count_dict = char_counter(text_full)
     sorted_list = chars_to_sorted_list(count_dict)
